70f0685789b250023d90d0de20dbb56b13156a3a
Juan Erazo <fersot100@yahoo.com>
2018-01-25 23:54:22 -0800

    Basic server setup with express

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan'); //Middleware that logs requests
+const app = express();
+const router = require('./router');
+
+//App Setup
+//app.use() registers the argument as middleware
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
+router(app);
+
+//Server Setup
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port);
+console.log(`Server listening on port: ${port}`);
+
